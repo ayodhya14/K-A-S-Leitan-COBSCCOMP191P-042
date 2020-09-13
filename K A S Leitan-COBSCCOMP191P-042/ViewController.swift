f3a82abaa195b4aa5556598317eb98f673e00ca5
Ayodhya14 <ayodhya.sri96@gmail.com>
2020-09-14 02:22:13 +0530

    Initial Commit

---
+++ K A S Leitan-COBSCCOMP191P-042/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  K A S Leitan-COBSCCOMP191P-042
+//
+//  Created by Prasad Gunasekara on 9/14/20.
+//  Copyright © 2020 Ayodhya Leitan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
